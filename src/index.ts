e31ead82c1799b3c53c4dece8531df7730275cc9
Italo Tavares <italotlima@hotmail.com>
2022-09-22 19:22:01 -0300

    Organizando exports para melhor uso nas aplicações

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Macrochat from './Macrotchat';
 import * as Helper from './Helper';
 
-export { Macrochat, Helper };
+export * from './Macrotchat/Macrochat';
+export { Helper };
+export default Macrochat;
